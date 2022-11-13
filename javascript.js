59bd6a87986a13fa168a019acf37a5b82143fe40
miguelvalden <miguelvalden77@gmail.com>
2022-11-13 17:08:34 +0100

    repaso

---
+++ javascript.js
@@ -0,0 +1,42 @@
+// for
+    // 1. Haz una función que deletree mediante console.log() el string que recibirá como parámetro
+
+
+    // 2. Haz que ahora lo deletree hacia atrás
+
+
+    //for of
+    // 3. Haz una función que reciba un array como parámetro y mediante console.log() muestre por consola todos los elementos
+
+
+// 4. Haz una función que reciba dos string como parámetros, si el segundo parámetro es "upper" se debe devolver el primer string pasado a mayúscula. Si es "lower" se debe devolver pasado a minúscula.
+
+
+// 5. Haz una función que devuelva un número al azar entre 0 y el número que recibirá como parámetro.
+
+
+// 6. Haz una función que reciba como parámetro un array de nombres y devuelva uno al azar.
+
+
+// 7. Haz una función que reciba un texto (string) como primer parámetro y como segundo parámetro otro string. Si el segundo string se encuentra dentro del primero, entonces devuelve true, sino false.
+
+
+// 8. Haz una función que reciba como parámetro un array de números y devuelva el primero que sea par.
+
+
+// 9. Haz una función que reciba un string y lo devuelva al revés.
+
+
+// 10. Haz una función que reciba un array y lo devuelva del revés
+
+
+// 11. Haz una función que reciba un array y un segundo parámetro que será añadido al array. Debe devolverse dicho array
+
+
+// 12. Haz una función que reciba un array, un elemento a incluir en el array y un string. Si el string es "pre" se añade al principio, si es "post", se añade al final
+
+
+// 13. Haz una función que reciba un array y un string, si el string es "pre" se debe eliminar un elemento al inicio, si es "post" se debe eliminar a final.
+
+
+// 14. Haz una función que calcule la media de un array de números que se pasará como parámetro
